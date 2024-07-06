aceee868b44b2cb6bba6bba5d0a0cf3e9512da29
karwanjiru <168063764+karwanjiru@users.noreply.github.com>
2024-07-06 18:46:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,15 +20,13 @@ with st.sidebar:
             st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['OPENAI_API_KEY'] = openai_api_key
 
-    st.subheader('Models and parameters')
-    selected_model = st.selectbox('Choose a GPT-4 model', ['gpt-3.5-turbo', 'gpt-4'], key='selected_model')
     st.markdown('Talk to a 👩‍⚕️[qualified personnel](link to website here) today😊')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-# Display or clear chat messages
+# Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
@@ -46,12 +44,12 @@ if st.sidebar.button('New Chat'):
     st.session_state.messages = [{"role": "assistant", "content": "Hello. How may I assist you today?"}]
 
 # Function for generating OpenAI response
-def generate_openai_response(model, prompt_input):
+def generate_openai_response(prompt_input):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     response = openai.ChatCompletion.create(
-        model=model,
+        model="gpt-4",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant that only answers questions related to mental health and general health. Please do not respond to any other types of queries."}
+            {"role": "system", "content": "You are a helpful assistant that only answers questions related to mental health and general health. If a user asks a question outside of these topics, respond politely by explaining that you are only able to assist with mental health and general health inquiries."}
         ] + st.session_state.messages + [{"role": "user", "content": prompt_input}]
     )
     return response['choices'][0]['message']['content']
@@ -66,6 +64,7 @@ if prompt := st.chat_input(disabled=not openai_api_key):
     if st.session_state.messages[-1]["role"] == "user":
         with st.chat_message("assistant"):
             with st.spinner("..."):
-                response = generate_openai_response(selected_model, prompt)
+                response = generate_openai_response(prompt)
                 st.write(response)  # Display the response
                 st.session_state.messages.append({"role": "assistant", "content": response})
+
